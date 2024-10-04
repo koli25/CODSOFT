ceebdb13ae08a3429709dcac5c45acb5db333335
koli25 <kolitushar060@gmail.com>
2024-10-04 16:21:41 +0530

    Initial Commit

--- app/src/main/java/com/example/todolistapp/AddEditTaskActivity.java
+++ app/src/main/java/com/example/todolistapp/AddEditTaskActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
